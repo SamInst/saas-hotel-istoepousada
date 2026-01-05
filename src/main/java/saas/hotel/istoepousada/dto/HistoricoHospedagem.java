84c5f941e967ad4066380746d73debb3845f02fe
Sam Helson Nunes Diniz <sam04hel@gmail.com>
2026-01-05 14:42:37 -0300

    refatorado record de historico de hospedagem

--- src/main/java/saas/hotel/istoepousada/dto/HistoricoHospedagem.java
+++ src/main/java/saas/hotel/istoepousada/dto/HistoricoHospedagem.java
@@ -1,41 +1,27 @@
 package saas.hotel.istoepousada.dto;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public record HistoricoHospedagem(
-        Long pessoaId,
-        LocalDate dataEntrada,
-        LocalDate dataSaida,
-        Float total,
-        List<DiariaHistorico> diarias
+        String tipoHospedagem,
+        Integer quantidadeHospedagens,
+        Integer totalDiasHospedado,
+        Float valorTotal,
+        List<DadosPernoite> pernoites
 ) {
 
-    public record DiariaHistorico(
-            Integer numeroDiaria,
-            LocalDate dataInicio,
-            LocalDate dataFim,
-            Integer quantidadePessoas,
-            String observacao,
-            Float subtotal,
-            List<PessoaResumo> pessoas,
-            List<SuiteResumo> suites,
-            List<PagamentoResumo> pagamentos,
-            List<ConsumoResumo> consumos
-    ) {}
-
-    public record PessoaResumo(Long id, String nome, Boolean representante) {}
-
-    public record SuiteResumo(Long pernoiteId, Quarto quarto, Float valor, PessoaResumo responsavel) {}
-
-    public record PagamentoResumo(Long id, Float valor, java.time.LocalDateTime dataHora, TipoPagamento tipoPagamento) {}
-
-    public record ConsumoResumo(
-            Long id,
-            java.time.LocalDateTime dataHora,
-            Item item,
-            Integer quantidade,
-            Float valorTotal,
-            TipoPagamento tipoPagamento
-    ) {}
+    public record DadosPernoite(
+            Pernoite pernoite,
+            List<DadosDiaria> dadosDiariaList,
+            Float valorTotalHospedagem
+    ) {
+        public record DadosDiaria(
+                Diaria diaria,
+                Pessoa representante,
+                List<Pessoa> acompanhantes,
+                List<DiariaPagamento> pagamentos,
+                List<DiariaConsumo> consumos,
+                Float subTotal
+        ) {}
+    }
 }
