05d327afcd732afb684b35c71fc8d80badb9abc0
Sam Helson Nunes Diniz <sam04hel@gmail.com>
2026-01-05 14:48:49 -0300

    agora é enum texto

--- src/main/java/saas/hotel/istoepousada/enums/StatusQuarto.java
+++ src/main/java/saas/hotel/istoepousada/enums/StatusQuarto.java
@@ -4,25 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum StatusQuarto {
-  OCUPADO(1),
-  DISPONIVEL(2),
-  RESERVADO(3),
-  LIMPEZA(4),
-  DIARIA_ENCERRADA(5),
-  MANUTENCAO(6);
-
-  private final int codigo;
-
-  StatusQuarto(int codigo) {
-    this.codigo = codigo;
-  }
-
-  public static StatusQuarto fromCodigo(int codigo) {
-    for (StatusQuarto status : StatusQuarto.values()) {
-      if (status.getCodigo() == codigo) {
-        return status;
-      }
-    }
-    throw new IllegalArgumentException("Código inválido: " + codigo);
-  }
+  OCUPADO,
+  DISPONIVEL,
+  RESERVADO,
+  LIMPEZA,
+  DIARIA_ENCERRADA,
+  MANUTENCAO
 }
