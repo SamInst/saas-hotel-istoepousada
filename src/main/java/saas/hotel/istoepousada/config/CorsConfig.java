4c7de2f09ae1de9348912de5886bede8254151f5
Sam Helson Nunes Diniz <sam04hel@gmail.com>
2026-01-16 20:44:26 -0300

    adicionando configuração de cors

--- src/main/java/saas/hotel/istoepousada/config/CorsConfig.java
+++ src/main/java/saas/hotel/istoepousada/config/CorsConfig.java
@@ -6,17 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:5173",
-                        "https://SEU-FRONT-DOMINIO.com"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("*");
-        // .allowCredentials(true); //cookie/sessão
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins(
+            "http://localhost:3000", "http://localhost:5173", "https://SEU-FRONT-DOMINIO.com")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .exposedHeaders("*");
+    // .allowCredentials(true); //cookie/sessão
+  }
 }
