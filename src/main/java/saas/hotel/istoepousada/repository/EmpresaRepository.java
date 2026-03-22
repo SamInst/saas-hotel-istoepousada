fb6999b4c52b7760d8088e41a02d2ae033ab7010
Sam Helson Nunes Diniz <sam04hel@gmail.com>
2026-03-21 21:09:46 -0300

    adicionando correções dos relatorios

--- src/main/java/saas/hotel/istoepousada/repository/EmpresaRepository.java
+++ src/main/java/saas/hotel/istoepousada/repository/EmpresaRepository.java
@@ -33,7 +33,7 @@ public class EmpresaRepository {
     List<Object> params = new ArrayList<>();
 
     if (has_id) {
-      where.append(" AND e.uuid = ? ");
+      where.append(" AND e.id = ? ");
       params.add(id);
     }
 
@@ -126,7 +126,7 @@ public class EmpresaRepository {
 
   public Optional<Empresa> findById(Long id) {
     Page<Empresa> page = findByIdNomeOuCnpj(id, null, Pageable.ofSize(1));
-    if (page.isEmpty()) throw new NotFoundException("Empresa não cadastrada para o uuid: " + id);
+    if (page.isEmpty()) throw new NotFoundException("Empresa não cadastrada para o id: " + id);
 
     return Optional.of(page.getContent().getFirst());
   }
